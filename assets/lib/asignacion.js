acb10bb4a26ccc16b2350ab67bbaab89317e9a78
johanC <johins2x@gmail.com>
2018-11-22 23:03:43 -0500

    ADD

--- assets/lib/asignacion.js
+++ assets/lib/asignacion.js
@@ -66,7 +66,50 @@ var asignacion = (() => {
         });
     };
 
+    self.obtenerPorId = (idMap,is_deleted) => {
+        self.op = "4";
+        $.ajax({
+            type:"POST",
+            data:{
+                op : self.op,
+                idMap:idMap
+            },
+            url:"../../../controller/MapController.php",
+            success:function(response){
+                var data = JSON.parse(response);
+                $("#actividad_proyecto").val(data[0].actividad);
+                $("#lat_proyecto").val(parseFloat(data[0].x));
+                $("#long_proyecto").val(parseFloat(data[0].y));
+                $("#codigo_proyecto").val(data[0].solicitud);
+                $("#contratista_proyecto").val(data[0].nombreContratista);
+                $("#cliente_proyecto").val(data[0].cliente);
+                //$("#fecini_proyecto").val(data[0].y);
+                //$("#fecfin_proyecto").val(data[0].y);
+                $("#direccion_proyecto").val(data[0].dirObra);
+                $("#distrito_proyecto").val(data[0].distrito);
+
+                self.lat = data[0].x;
+                self.lng = data[0].y;
+                self.name = data[0].dirObra;
+                self.initMap();
+
+                $("#new_proyecto").modal("show");
+
+            }
+        });
+        if(is_deleted){
+            
+        }
+    };
+
     self.loadTable = () => {
+        $("#tableMap tbody").empty().append(`<tr>
+                                        <td colspan="6">
+                                            <center>
+                                                <img src="../../../../assets/img/ajax_loader.gif"/>
+                                            </center>
+                                        </td>
+                                    </tr>`);
         self.op = "23";
         $.ajax({
             type:"POST",
@@ -75,8 +118,43 @@ var asignacion = (() => {
             },
             async: false,
             url:"../../../controller/MapController.php",
-            success:function(msg){
-                console.log(msg);
+            success:function(response){
+                var tbody = '';
+                var data = JSON.parse(response);
+                if(data.length > 0){
+                    for(var i = 0;i < data.length;i++){
+                        tbody += `<tr>
+                                    <td><center>`  + data[i].solicitud + `</center></td>
+                                    <td><center>`  + data[i].cliente + `</center></td>
+                                    <td><center>`  + data[i].nombreContratista + `</center></td>
+                                    <td><center>`  + data[i].actividad + `</center></td>
+                                    <td><center>`  + data[i].dirObra + `</center></td>
+                                    <td>
+                                        <center>
+                                            <a href="javascript:void(0);" onclick="asignacion.obtenerPorId(` + data[i].idMap + `,false);">
+                                                <img src="../../../../assets/img/edit.gif" style="width:15px;">
+                                            </a>
+                                        </center>
+                                    </td>
+                                    <td>
+                                        <center>
+                                            <a href="javascript:void(0);" onclick="asignacion.obtenerPorId(` + data[i].idMap + `,false);">
+                                                <img src="../../../../assets/img/cancelar.png" style="width:15px;">
+                                            </a>
+                                        </center>
+                                    </td>
+                                </tr>`;
+                    }
+                    if(tbody !== ''){
+                        $("#tableMap tbody").empty().append(tbody);
+                    }else{
+                        $("#tableMap tbody").empty().append(`<tr>
+                                                                <td colspan="6">
+                                                                    No se encontraron resultados
+                                                                </td>
+                                                            </tr>`);
+                    }
+                }
             }
         });
     };
--- com/vistas/mantenimientos/asignacion/asignacion.php
+++ com/vistas/mantenimientos/asignacion/asignacion.php
@@ -17,7 +17,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                
                 <div class="panel-group" id="accordion">
                     <div class="panel panel-default">
                         <div class="panel-heading">
@@ -41,99 +40,149 @@
                         </div>
                     </div> 
                 </div>
-                <!-- =========================================  MODAL GUARDAR MAPA ===================================== -->
-                <div id="new_proyecto" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" style="overflow-y: scroll;">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">Nuevo Proyecto</h4>
-                            </div>
-                            <div class="modal-body pac-container">
-                                <form id="frm_map" role="form" >
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="col-md-8">
-                                                <div class="form-group">
-                                                    <label>Actividad</label>
-                                                    <input type="text" name="actividad_proyecto" id="actividad_proyecto" class="form-control"/>
-                                                    <input type="hidden" name="lat_proyecto" id="lat_proyecto"/>
-                                                    <input type="hidden" name="long_proyecto" id="long_proyecto"/>
-                                                    <input type="hidden" name="op" id="op" value="22"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <div class="form-group">
-                                                    <label>Codigo</label>
-                                                    <input type="text" name="codigo_proyecto" id="codigo_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="col-md-4">
-                                                <div class="form-group">
-                                                    <label>Contratista</label>
-                                                    <input type="text" name="contratista_proyecto" id="contratista_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-8">
-                                                <div class="form-group">
-                                                    <label>Cliente</label>
-                                                    <input type="text" name="cliente_proyecto" id="cliente_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Fecha Inicio</label>
-                                                    <input type="date" name="fecini_proyecto" id="fecini_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label>Fecha Fin</label>
-                                                    <input type="date" name="fecfin_proyecto" id="fecfin_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-8">
-                                                <div class="form-group">
-                                                    <label>Dirección</label>
-                                                    <input type="text" name="direccion_proyecto" id="direccion_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <div class="form-group">
-                                                    <label>Distrito</label>
-                                                    <input type="text" name="distrito_proyecto" id="distrito_proyecto" class="form-control"/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <center>
-                                                <div id="map_proyecto" style="width: 550px; height: 200px"></div>
-                                            </center>
-                                        </div>
-                                    </div>
-                                </form>
-                                <br/>
-                                <button id="btn_clean_map" type="button" class="btn">
-                                    Cancelar
-                                </button>
-                                <button id="btn_save_map" type="button" class="btn btn-primary">
-                                    Guardar
-                                </button>
-                                <div id="res_map"></div>
-                            </div>
-                        </div> 
-                    </div>
-                </div>
-                <!-- ========================================= / MODAL GUARDAR MAPA ===================================== -->
-
+            </div>
+            <div class="col-md-12">
+                <table id="tableMap" class='table table-striped table-bordered table-hover'>
+                    <thead>
+                        <tr>
+                            <th>
+                                <center>
+                                    Solicitud
+                                </center>
+                            </th>
+                            <th>
+                                <center>
+                                    Cliente
+                                </center>
+                            </th>
+                            <th>
+                                <center>
+                                    Contratista
+                                </center>
+                            </th>
+                            <th>
+                                <center>
+                                    Actividad
+                                </center>
+                            </th>
+                            <th>
+                                <center>
+                                    Dirección
+                                </center>
+                            </th>
+                            <th colspan="2">
+                                <center>
+                                    Acciones
+                                </center>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+                <center>
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">50</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav>
+                </center>
             </div>
         </div>
+
+        <!-- =========================================  MODAL GUARDAR MAPA ===================================== -->
+        <div id="new_proyecto" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" style="overflow-y: scroll;">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Nuevo Proyecto</h4>
+                    </div>
+                    <div class="modal-body pac-container">
+                        <form id="frm_map" role="form" >
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label>Actividad</label>
+                                            <input type="text" name="actividad_proyecto" id="actividad_proyecto" class="form-control"/>
+                                            <input type="hidden" name="lat_proyecto" id="lat_proyecto"/>
+                                            <input type="hidden" name="long_proyecto" id="long_proyecto"/>
+                                            <input type="hidden" name="op" id="op" value="22"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Codigo</label>
+                                            <input type="text" name="codigo_proyecto" id="codigo_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Contratista</label>
+                                            <input type="text" name="contratista_proyecto" id="contratista_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label>Cliente</label>
+                                            <input type="text" name="cliente_proyecto" id="cliente_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Fecha Inicio</label>
+                                            <input type="date" name="fecini_proyecto" id="fecini_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Fecha Fin</label>
+                                            <input type="date" name="fecfin_proyecto" id="fecfin_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label>Dirección</label>
+                                            <input type="text" name="direccion_proyecto" id="direccion_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Distrito</label>
+                                            <input type="text" name="distrito_proyecto" id="distrito_proyecto" class="form-control"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <center>
+                                        <div id="map_proyecto" style="width: 550px; height: 200px"></div>
+                                    </center>
+                                </div>
+                            </div>
+                        </form>
+                        <br/>
+                        <button id="btn_clean_map" type="button" class="btn">
+                            Cancelar
+                        </button>
+                        <button id="btn_save_map" type="button" class="btn btn-primary">
+                            Guardar
+                        </button>
+                        <div id="res_map"></div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <!-- ========================================= / MODAL GUARDAR MAPA ===================================== -->
+
     </div>
     <body>
         <?php
